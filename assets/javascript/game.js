85f554a19984cdd639bfa3635c255c81c3011ae9
Lowden Hall <lowdenhall@gmail.com>
2019-09-24 23:45:51 -0500

    Adding content

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,12 @@ var resetAndStart = function () {
 
     $(".crystals").empty();
 
-    var images = []
+    var images = [
+        <img src="images/DIAMOND.jpg"></img>
+        <img src="images/EMERALD.jpg"></img>
+        <img src="images/RUBY.jpg"></img>
+
+    ]
 
     random_result = Math.floor(Math.random() * 69 ) + 30;
 
@@ -24,9 +29,72 @@ var resetAndStart = function () {
                 "class": 'crystal',
                 "data-random": random
             });
+            crystal.css({
+                "background-image":"url('" + images[i] + "')",
+                "background-size":"cover"
+            
+            });
 
 
         $(".crystals").appened(crystal);
 
 
+        }
+
+        $("#previous").html("Total Score: " + previous);
+
+    }
+
+resetAndStart();
+
+
+$(document).on('click', ".crystal", function () {
+
+    var num = parseInt($(this).attr('data-random'));
+
+    previous += num;
+
+
+    $("#previous").html("Total Score: " + previous);
+
+    
 }
+
+resetAndStart();
+
+$(document).on('click', ".crystal", function () {
+
+	var num = parseInt($(this).attr('data-random'));
+
+	previous += num;
+
+
+	$("#previous").html("Total score: " + previous);
+
+	console.log(previous);
+
+	if(previous > random_result){
+
+		lost++;
+
+		$("#lost").html("You lost: " + lost);
+
+		previous = 0;
+
+		resetAndStart();
+
+	} 
+	else if(previous === random_result){
+
+		win++;
+
+		$("#win").html("You win: " + win);
+
+		previous = 0;
+
+		resetAndStart();
+
+	}
+
+}});
+
